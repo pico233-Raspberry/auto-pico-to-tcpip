ba567594471ec269607af6812a28db42f2ee6649
pico233-Raspberry <17505937757@163.com>
2025-06-15 20:01:56 +0800

    Update main.c

--- adb_host/main.c
+++ adb_host/main.c
@@ -1,8 +1,7 @@
-
-#include <stdio.h>
-#include "pico/pio_usb.h"  // 包含USB配置结构体
+// main.c 和 adb.c 的顶部修改为：
+#include <stdint.h>      // 添加标准类型定义
+#include "pio_usb.h"     // 修改为直接包含 PIO-USB 的头文件
 #include "pico/stdlib.h"
-#include "pio_usb.h"
 #include "adb.h"
 
 int main() {
