befa3abab1533056a04d985c08c3163d01e115a8
pico233-Raspberry <17505937757@163.com>
2025-06-21 11:38:44 +0800

    Update adb.h

--- adb_host/adb.h
+++ adb_host/adb.h
@@ -1,10 +1,12 @@
 #ifndef ADB_H
 #define ADB_H
-
 #include <stdbool.h>
+#include "pio_usb.h"
+
+uint32_t usb_host_get_device_count(void);
+void usb_host_write(uint8_t endpoint, const void* data, uint32_t len);
 
 void adb_init();
 bool adb_device_connected();
 void adb_enable_tcpip();
-
 #endif
